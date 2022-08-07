f8b15d56cc07910eaf5acba7bfa8ef018571c7a0
Saulo Pereira <saulopsg@hotmail.com>
2022-08-07 17:02:23 -0400

    Nodemon

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/',(req, res) => {
+    res.send('Hello Sebrae');
+});
+
+app.listen(PORT, HOST);
